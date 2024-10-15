bd561b97eb82a45e81b8e2d39cdc8a040804f412
Victoria Usan <victoria.usan@gmail.com>
2024-10-15 14:02:45 +0200

    Adding Page Objects

---
+++ automated-scenarios/page-objects/searchPage.ts
@@ -0,0 +1,79 @@
+import { expect, type Locator, type Page } from "@playwright/test";
+
+export class SearchPage {
+	readonly page: Page;
+	readonly searchField: Locator;
+	readonly searchAction: Locator;
+	readonly item: Locator;
+	readonly itemCard: Locator;
+	readonly itemDetail: Locator;
+	readonly closeItemDetail: Locator;
+	readonly addItem: Locator;
+	readonly itemNumber: Locator;
+
+	constructor(page: Page) {
+		this.page = page;
+		this.searchField = page.locator("#searchGlobal");
+		this.searchAction = page.locator("[type='submit']");
+		this.itemCard = page.locator('[data-test="productCard"]');
+		this.itemDetail = page.locator("#productDetail");
+		this.closeItemDetail = page.locator(
+			'div[role="button"][class*="modal-close-class"]',
+		);
+		this.addItem = page.locator('button[data-gtm-button="addPiece"]');
+		this.itemNumber = page.locator('span[data-test="lblCount"]');
+	}
+
+	async visitHomepage() {
+		await this.page.goto("https://www.rohlik.cz/?hp=true");
+	}
+
+	async whatIsURL() {
+		return this.page.url();
+	}
+
+	async compareURLs(url: string) {
+		await expect(this.page).toHaveURL(url);
+	}
+
+	async fillInItem(item: string) {
+		await this.searchField.fill(item);
+	}
+
+	async lookForItem() {
+		await this.searchAction.click();
+	}
+
+	async findItem(item: string) {
+		await this.fillInItem(item);
+		await this.lookForItem();
+	}
+
+	async verifyResults(item: string) {
+		await expect(this.page.getByText(item)).toBeVisible();
+	}
+
+	async clickFirstExistingItem() {
+		await this.itemCard.first().click();
+	}
+
+	async verifyItemDetails() {
+		await expect(this.itemDetail).toBeVisible();
+	}
+
+	async closeDetails() {
+		await this.closeItemDetail.click();
+	}
+
+	async verifyClosedDetails() {
+		await expect(this.itemDetail).not.toBeVisible();
+	}
+
+	async addToCart() {
+		await this.addItem.click();
+	}
+
+	async verifyItemInCart() {
+		await expect(this.itemNumber).toBeVisible();
+	}
+}
--- automated-scenarios/tests/searching-items.spec.ts
+++ automated-scenarios/tests/searching-items.spec.ts
@@ -1,60 +1,62 @@
-import { test, expect } from "@playwright/test"
-import { clickFirstExistingItem, closeDetails, findItem, loadHomepage, verifyItemDetails, verifyResults, verifyClosedDetails, addToCart, verifyItemInCart } from "../helpers/item-actions";
+import { test } from "@playwright/test";
+import { SearchPage } from "../page-objects/searchPage";
 
 test.describe("Searching for items", () => {
+	let searchPage: SearchPage;
 
-    test.beforeEach("Load homepage", async ({ page }) => {
-        await loadHomepage(page);
-    })
+	test.beforeEach("Load homepage", async ({ page }) => {
+		searchPage = new SearchPage(page);
+		searchPage.visitHomepage();
+	});
 
-    test("User searches for existing item and can open item", async ({ page }) => {
-        await findItem(page, "Cibule");
-        await verifyResults(page, '"Cibule"');
-        await clickFirstExistingItem(page);
-        await verifyItemDetails(page);
-    })
+	test("User searches for existing item and can open item", async () => {
+		searchPage.findItem("Cibule");
+		searchPage.verifyResults('"Cibule');
+		searchPage.clickFirstExistingItem();
+		searchPage.verifyItemDetails();
+	});
 
-    test("User searches for non-existing invalid item", async ({ page }) => {
-        await findItem(page, "nsnfdkjs");
-        await verifyResults(page, '"nsnfdkjs"');
-    })
+	test("User searches for non-existing invalid item", async () => {
+		searchPage.findItem("nsnfdkjs");
+		searchPage.verifyResults('"nsnfdkjs"');
+	});
 
-    test("User searches for non-existing valid item", async ({ page }) => {
-        await findItem(page, "Pocitadlo");
-        await verifyResults(page, '"Pocitadlo"');
-    })
+	test("User searches for non-existing valid item", async () => {
+		searchPage.findItem("Pocitadlo");
+		searchPage.verifyResults('"Pocitadlo"');
+	});
 
-    test("User searches for empty item", async ({ page }) => {
-        await page.fill("#searchGlobal", "");
-        const url = page.url();
-        await page.locator("[type='submit']").click();
-        await expect(page).toHaveURL(url)
-    })
+	test("User searches for empty item", async () => {
+		searchPage.fillInItem("");
+		const url = searchPage.whatIsURL();
+		searchPage.lookForItem();
+		searchPage.compareURLs(await url);
+	});
 
-    test("User can change search", async ({ page }) => {
-        await findItem(page, "Cibule");
-        await verifyResults(page, '"Cibule"');
-        await findItem(page, "nsnfdkjs");
-        await verifyResults(page, '"nsnfdkjs"');
-    })
+	test("User can change search", async () => {
+		searchPage.findItem("Cibule");
+		searchPage.verifyResults('"Cibule"');
+		searchPage.findItem("nsnfdkjs");
+		searchPage.verifyResults('"nsnfdkjs"');
+	});
 
-    test("user can continue browsing", async ({ page }) => {
-        await findItem(page, "Cibule");
-        await verifyResults(page, '"Cibule"');
-        await clickFirstExistingItem(page);
-        await verifyItemDetails(page);
-        await closeDetails(page);
-        await verifyClosedDetails(page);
-    })
+	test("User can continue browsing", async () => {
+		searchPage.findItem("Cibule");
+		searchPage.verifyResults('"Cibule"');
+		searchPage.clickFirstExistingItem();
+		searchPage.verifyItemDetails();
+		searchPage.closeDetails();
+		searchPage.verifyClosedDetails();
+	});
 
-    test("User can continue search after item has been added to cart", async ({ page }) => {
-        await findItem(page, "Cibule");
-        await verifyResults(page, '"Cibule"');
-        await clickFirstExistingItem(page);
-        await verifyItemDetails(page);
-        await addToCart(page);
-        await verifyItemInCart(page);
-        await closeDetails(page);
-        await verifyClosedDetails(page);
-    })
-})
+	test("User can continue search after item has been added to cart", async () => {
+		searchPage.findItem("Cibule");
+		searchPage.verifyResults('"Cibule"');
+		searchPage.clickFirstExistingItem();
+		searchPage.verifyItemDetails();
+		searchPage.addToCart();
+		searchPage.verifyItemInCart();
+		searchPage.closeDetails();
+		searchPage.verifyClosedDetails();
+	});
+});
